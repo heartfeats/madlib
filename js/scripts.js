ebb1b6e8b2045262542ffa12060a1cacb97b0860
jm <heartfeats@gmail.com>
2017-09-21 12:05:48 -0700

    Add forEach loop to replace redundant js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,34 @@
 
 $(function() {
   $("#form-one").submit(function(event) {
-    var lastNameInput = $("input#last-name").val();
-    var illnessInput = $("input#illness").val();
-    var nounInput = $("input#noun").val();
-    var adjOneInput = $("input#adjective-one").val();
-    var adjTwoInput = $("input#adjective-two").val();
-    var sillyInput = $("input#silly").val();
-    var placeInput = $("input#place").val();
-    var numberInput = $("input#number").val();
 
-    $(".last-name").text(lastNameInput);
-    $(".illness").text(illnessInput);
-    $(".noun").text(nounInput);
-    $(".adjective-one").text(adjOneInput);
-    $(".adjective-two").text(adjTwoInput);
-    $(".silly").text(sillyInput);
-    $(".place").text(placeInput);
-    $(".number").text(numberInput);
-    // $(".last-name").text(last-name-input);
+    var blanks = ['last-name', 'illness', 'adjective-one', 'adjective-two', 'silly', 'place', 'number']
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val()
+      $("." + blank).text(userInput)
+    });
+
+    // (Above forEach loop returns the same result as below)
+    //
+    // 
+    // var lastNameInput = $("input#last-name").val();
+    // var illnessInput = $("input#illness").val();
+    // var nounInput = $("input#noun").val();
+    // var adjOneInput = $("input#adjective-one").val();
+    // var adjTwoInput = $("input#adjective-two").val();
+    // var sillyInput = $("input#silly").val();
+    // var placeInput = $("input#place").val();
+    // var numberInput = $("input#number").val();
+    //
+    // $(".last-name").text(lastNameInput);
+    // $(".illness").text(illnessInput);
+    // $(".noun").text(nounInput);
+    // $(".adjective-one").text(adjOneInput);
+    // $(".adjective-two").text(adjTwoInput);
+    // $(".silly").text(sillyInput);
+    // $(".place").text(placeInput);
+    // $(".number").text(numberInput);
 
     $("#note").show();
 
